2ea73509a1ba0c0fa4698ef127a4056f6b3331d3
Oliver <yi721003@gmail.com>
2025-04-09 16:13:15 +0800

    feat(product): 設定產品資訊為輸入型屬性

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
@@ -7,13 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './product-card.component.scss',
 })
 export class ProductCardComponent {
-  productName = '書籍 A';
-  author = '作者甲、作者乙、作者丙';
-  company = '博碩文化';
+  @Input()
+  productName!: string;
 
-  isShow = true;
+  @Input()
+  author!: string;
 
-  photoUrl = 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img';
+  @Input()
+  company!: string;
+
+  @Input()
+  isShow!: boolean;
+
+  @Input()
+  photoUrl!: string;
 
   onSetDisplay(isShow: boolean): void {
     this.isShow = isShow;
