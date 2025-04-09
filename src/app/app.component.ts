c5c7bbf8893d201925031efccf72056d38bee45f
Oliver <yi721003@gmail.com>
2025-04-09 17:19:09 +0800

    feat(product): 建立產品卡片清單元件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,10 @@
 import { Component } from '@angular/core';
-import { ProductCardComponent } from './product-card/product-card.component';
+import { ProductCardListComponent } from './product-card-list/product-card-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ProductCardComponent],
+  imports: [ProductCardListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  productName = '書籍 A';
-  author = '作者甲、作者乙、作者丙';
-  company = '博碩文化';
-
-  isShow = true;
-
-  photoUrl = 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img';
-
-  createDate = new Date('2025/4/9');
-
-  price = 10000;
-
-  onSetDisplay(isShow: boolean): void {
-    this.isShow = isShow;
-  }
-}
+export class AppComponent {}
--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,5 +1,5 @@
 import { CurrencyPipe, DatePipe } from '@angular/common';
-import { Component, EventEmitter, Input, Output, booleanAttribute, numberAttribute } from '@angular/core';
+import { Component, EventEmitter, HostBinding, Input, Output, booleanAttribute, numberAttribute } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
@@ -34,6 +34,9 @@ export class ProductCardComponent {
 
   @Input({ transform: numberAttribute })
   price!: number;
+
+  @HostBinding('class')
+  class = 'app-product-card';
 
   onSetDisplay(isShow: boolean): void {
     this.isShowChange.emit(isShow);
