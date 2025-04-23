16deb488850ccf565fe907e997436e6556cf6844
Oliver <yi721003@gmail.com>
2025-04-23 18:15:45 +0800

    feat(product-form): 切換產品表單頁面實作

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -65,7 +65,11 @@ export class ProductPageComponent {
     }),
   ];
 
+  onEdit(product: Product): void {
+    this.router.navigate(['product', 'form', product.id]);
+  }
+
   onView(product: Product): void {
-    this.router.navigate(['product', product.id]);
+    this.router.navigate(['product', 'view', product.id]);
   }
 }
