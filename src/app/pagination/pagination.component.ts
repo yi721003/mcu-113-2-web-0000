f94b8cb1d8638f2933ed67c2e9a27c16d020b836
Oliver <yi721003@gmail.com>
2025-04-30 16:45:17 +0800

    feat(product): 實作產品資料分頁作業

--- src/app/pagination/pagination.component.ts
+++ src/app/pagination/pagination.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, computed, input, model, numberAttribute, output, signal } from '@angular/core';
+import { Component, computed, input, model, numberAttribute } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
@@ -11,7 +11,7 @@ export class PaginationComponent {
 
   readonly pageSize = input.required<number, string | number>({ transform: numberAttribute });
 
-  readonly pageIndex = model.required();
+  readonly pageIndex = model.required<number>();
 
   readonly range = computed(() => {
     const length = Math.ceil(this.totalCount() / this.pageSize());
--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -3,10 +3,11 @@ import { Router } from '@angular/router';
 import { ProductCardListComponent } from '../product-card-list/product-card-list.component';
 import { Product } from '../models/product';
 import { ProductService } from '../services/product.service';
+import { PaginationComponent } from '../pagination/pagination.component';
 
 @Component({
   selector: 'app-product-page',
-  imports: [ProductCardListComponent],
+  imports: [PaginationComponent, ProductCardListComponent],
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.scss',
 })
@@ -15,10 +16,16 @@ export class ProductPageComponent implements OnInit {
 
   private productService = inject(ProductService);
 
+  pageIndex = 1;
+
+  pageSize = 5;
+
+  totalCount = 0;
+
   products: Product[] = [];
 
   ngOnInit(): void {
-    this.products = this.productService.getList();
+    this.getProducts();
   }
 
   onEdit(product: Product): void {
@@ -27,5 +34,16 @@ export class ProductPageComponent implements OnInit {
 
   onView(product: Product): void {
     this.router.navigate(['product', 'view', product.id]);
+  }
+
+  onPageIndexChange(pageIndex: number): void {
+    this.pageIndex = pageIndex;
+    this.getProducts();
+  }
+
+  private getProducts(): void {
+    const { data, count } = this.productService.getList(undefined, this.pageIndex, this.pageSize);
+    this.products = data;
+    this.totalCount = count;
   }
 }
