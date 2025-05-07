941a536a46145f6d86cff2dfc447b40da24db51f
Oliver <yi721003@gmail.com>
2025-05-07 18:20:26 +0800

    feat(resolver): 建立產品資料預載資料

--- src/app/product-detail-page/product-detail-page.component.ts
+++ src/app/product-detail-page/product-detail-page.component.ts
@@ -1,5 +1,5 @@
 import { CurrencyPipe } from '@angular/common';
-import { Component, OnInit, inject, input, numberAttribute } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { Router } from '@angular/router';
 import { Product } from '../models/product';
 import { ProductService } from '../services/product.service';
@@ -10,25 +10,19 @@ import { ProductService } from '../services/product.service';
   templateUrl: './product-detail-page.component.html',
   styleUrl: './product-detail-page.component.scss',
 })
-export class ProductDetailPageComponent implements OnInit {
-  id = input.required<number, string | number>({ transform: numberAttribute });
-
-  product!: Product;
+export class ProductDetailPageComponent {
+  readonly product = input.required<Product>();
 
   readonly router = inject(Router);
 
   private productService = inject(ProductService);
 
-  ngOnInit(): void {
-    this.productService.getById(this.id()).subscribe((product) => (this.product = product));
-  }
-
   onEdit(): void {
-    this.router.navigate(['product', 'form', this.product.id]);
+    this.router.navigate(['product', 'form', this.product().id]);
   }
 
   onRemove(): void {
-    this.productService.remove(this.product.id);
+    this.productService.remove(this.product().id);
     this.router.navigate(['products']);
   }
 
