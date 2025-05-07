c3af78cd87246690f1b7dcf41850da049b07d81d
Oliver <yi721003@gmail.com>
2025-05-07 18:25:36 +0800

    faet(product): 產品表單頁面使用預載資料

--- src/app/product-form-page/product-form-page.component.ts
+++ src/app/product-form-page/product-form-page.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute, Data } from '@angular/router';
+import { map } from 'rxjs';
+import { Product } from '../models/product';
 
 @Component({
   selector: 'app-product-form-page',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './product-form-page.component.html',
-  styleUrl: './product-form-page.component.scss'
+  styleUrl: './product-form-page.component.scss',
 })
-export class ProductFormPageComponent {
+export class ProductFormPageComponent implements OnInit {
+  private readonly route = inject(ActivatedRoute);
 
+  product!: Product;
+
+  ngOnInit(): void {
+    this.route.data.pipe(map(({ product }: Data) => product)).subscribe((product) => (this.product = product));
+  }
 }
