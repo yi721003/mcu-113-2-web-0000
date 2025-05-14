ecbdd1272af96bcf0e3a902efa0ac8d929130942
Oliver <yi721003@gmail.com>
2025-05-14 17:08:27 +0800

    feat(product): 實作從後端服務取得所有產品資料

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { ProductRemoteService } from './services/product-remote.service';
+import { ProductService } from './services/product.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withComponentInputBinding())],
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(),
+    provideRouter(routes, withComponentInputBinding()),
+    { provide: ProductService, useClass: ProductRemoteService },
+  ],
 };
