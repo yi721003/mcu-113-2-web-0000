83ac7a1354895fc8f1606050fae8e4149450134f
Oliver <yi721003@gmail.com>
2025-05-21 17:19:19 +0800

    feat(product-form): 產品表單新增作業實作

--- src/app/product-form-page/product-form-page.component.ts
+++ src/app/product-form-page/product-form-page.component.ts
@@ -1,9 +1,10 @@
 import { JsonPipe } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
 import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { ActivatedRoute, Data } from '@angular/router';
+import { ActivatedRoute, Data, Router } from '@angular/router';
 import { map } from 'rxjs';
 import { Product } from '../models/product';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-product-form-page',
@@ -12,7 +13,11 @@ import { Product } from '../models/product';
   styleUrl: './product-form-page.component.scss',
 })
 export class ProductFormPageComponent implements OnInit {
+  private readonly router = inject(Router);
+
   private readonly route = inject(ActivatedRoute);
+
+  private readonly productService = inject(ProductService);
 
   form = new FormGroup({
     id: new FormControl<string | null>(null),
@@ -20,6 +25,7 @@ export class ProductFormPageComponent implements OnInit {
     authors: new FormArray<FormControl<string | null>>([]),
     company: new FormControl<string | null>(null, { validators: [Validators.required] }),
     price: new FormControl<number | null>(null, { validators: [Validators.required] }),
+    isShow: new FormControl<boolean>(true, { nonNullable: true }),
   });
 
   product!: Product;
@@ -36,6 +42,10 @@ export class ProductFormPageComponent implements OnInit {
     return this.form.get('company') as FormControl<string | null>;
   }
 
+  get isShow(): FormControl<boolean> {
+    return this.form.get('isShow') as FormControl<boolean>;
+  }
+
   get price(): FormControl<string | null> {
     return this.form.get('price') as FormControl<string | null>;
   }
@@ -47,5 +57,22 @@ export class ProductFormPageComponent implements OnInit {
   onAddAuthor(): void {
     const formControl = new FormControl<string | null>(null, { validators: [Validators.required] });
     this.authors.push(formControl);
+  }
+
+  onSave(): void {
+    const formData = new Product({
+      name: this.name.value!,
+      authors: this.authors.value.map((author) => author!),
+      company: this.company.value!,
+      isShow: this.isShow.value,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      createDate: new Date(),
+      price: +(this.price.value || '0'),
+    });
+    this.productService.add(formData).subscribe(() => this.router.navigate(['products']));
+  }
+
+  onCancel(): void {
+    this.router.navigate(['products']);
   }
 }
