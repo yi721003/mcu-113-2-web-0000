1a0569799c96b7a7bc171601f266895db9eb0487
Oliver <yi721003@gmail.com>
2025-05-21 16:29:19 +0800

    feat(product-form): 產品表單名稱、出版社、單價欄位設定

--- src/app/product-form-page/product-form-page.component.ts
+++ src/app/product-form-page/product-form-page.component.ts
@@ -1,17 +1,25 @@
 import { JsonPipe } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, Data } from '@angular/router';
 import { map } from 'rxjs';
 import { Product } from '../models/product';
 
 @Component({
   selector: 'app-product-form-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './product-form-page.component.html',
   styleUrl: './product-form-page.component.scss',
 })
 export class ProductFormPageComponent implements OnInit {
   private readonly route = inject(ActivatedRoute);
+
+  form = new FormGroup({
+    id: new FormControl<string | null>(null),
+    name: new FormControl<string | null>(null),
+    company: new FormControl<string | null>(null),
+    price: new FormControl<number | null>(null),
+  });
 
   product!: Product;
 
