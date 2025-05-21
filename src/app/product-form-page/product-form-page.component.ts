14f604bb033d59f8ed1f19051ab66882c51681c1
Oliver <yi721003@gmail.com>
2025-05-21 17:00:48 +0800

    feat(product-form): 實作產品作者表單輸入

--- src/app/product-form-page/product-form-page.component.ts
+++ src/app/product-form-page/product-form-page.component.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, Data } from '@angular/router';
 import { map } from 'rxjs';
 import { Product } from '../models/product';
@@ -17,13 +17,23 @@ export class ProductFormPageComponent implements OnInit {
   form = new FormGroup({
     id: new FormControl<string | null>(null),
     name: new FormControl<string | null>(null),
+    authors: new FormArray<FormControl<string | null>>([]),
     company: new FormControl<string | null>(null),
     price: new FormControl<number | null>(null),
   });
 
   product!: Product;
 
+  get authors(): FormArray<FormControl<string | null>> {
+    return this.form.get('authors') as FormArray<FormControl<string | null>>;
+  }
+
   ngOnInit(): void {
     this.route.data.pipe(map(({ product }: Data) => product)).subscribe((product) => (this.product = product));
+  }
+
+  onAddAuthor(): void {
+    const formControl = new FormControl<string | null>(null);
+    this.authors.push(formControl);
   }
 }
